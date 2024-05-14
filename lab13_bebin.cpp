f6f7a4feebb82bd16ec09c6704a7ead0df5b64e1
hhh <hhh@-1>
2024-05-15 01:52:20 +0300

    Добавьте файлы проекта.

---
+++ lab13_bebin.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+double seriesMember(int n);
+
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    int n;
+    cout << "Номер члена ряду n: ";
+    cin >> n;
+
+    cout << "Член ряду пiд номером " << n << " дорiвнює " << seriesMember(n) << endl;
+
+    return 0;
+}
+
+double seriesMember(int n) {
+    if (n == 1) {
+        return 3.0;
+    }
+    else {
+        return seriesMember(n - 1) * 0.1;
+    }
+}
